fd463595f892f24d372e1b2d1bfa5c2a321178d2
Glenpoint Alton <glenpoin@si-e3.kzn.21-school.ru>
2022-05-15 17:58:22 +0300

    upd

--- src/s21_math.c
+++ src/s21_math.c
@@ -1,15 +1,29 @@
 #include "s21_math.h"
 
 #include <math.h>
-#include <stdio.h>
 
 // 1
 int s21_abs(int x) { return (x < 0) ? -x : x; }
 // 2
-
+long double s21_acos(double x) {
+    return s21_PI/2-asin(x);
+}
 // 3
-
+long double s21_asin(double x) {
+    double tmp = x;
+    double result = x;
+    int status = 0;
+    x<-1 || x> 1 ? (result = s21_NAN), (status = 1) : (status = 0);
+    x == -1 || x == 1 ? (result = s21_PI / 2 * x), (status = 1) : (status = 0);
+    for (long double count = 1; count < 10000000 && status == 0; count++) {
+        tmp *= ((x * x) * (2 * count - 1) * (2 * count - 1)) /
+               ((2 * count) * (2 * count + 1));
+        result += tmp;
+    }
+    return result;
+}
 // 4
+long double s21_atan(double x) { return asin(x / (sqrt(1 + pow(x, 2)))); }
 
 // 5
 long double s21_ceil(double x) {
@@ -18,6 +32,7 @@ long double s21_ceil(double x) {
                : (double)((int)x);
 }
 // 6
+long double s21_cos(double x) { return s21_sin(s21_PI / 2 - x); }
 
 // 7
 long double s21_exp(double x) {
@@ -55,8 +70,7 @@ long double s21_fmod(double x, double y) {
 }
 // 11
 long double s21_log(double x) {
-    long double temp;
-    return temp = (x > 0 && x < 2) ? log_0_2(x) : log_other(x);
+    return (long double)(x > 0 && x < 2) ? log_0_2(x) : log_other(x);
 }
 
 // 12
@@ -64,12 +78,26 @@ long double s21_pow(double base, double exp) {
     return (long double)s21_exp(exp * s21_log(base));
 }
 // 13
+long double s21_sin(double x) {
+    x = delete (x);
+    long double result = x, temp = x;
+    long double i = 1.;
+    while (s21_fabs(result) > s21_EPS) {
+        result = -1 * result * x * x / (2 * i * (2 * i + 1));
+        i += 1.;
+        temp += result;
+    }
+    return temp;
+}
 
 // 14
 long double s21_sqrt(double x) {
     return s21_pow(S21_E, (long double)0.5 * s21_log((long double)x));
 }
 // 15
+long double s21_tan(double x) {
+    return (s21_sin(delete (x)) / s21_cos(delete (x)));
+}
 
 long double log_0_2(double x) {
     x--;
@@ -115,4 +143,19 @@ void translate(double x, struct special *_special) {
     _special->mantisa = x;
     _special->pow = i;
     _special->e = es;
+}
+
+double delete (double x) {
+    while (x > s21_PI || x < -s21_PI) {
+        x += x > s21_PI ? -2 * s21_PI : 2 * s21_PI;
+    }
+    return x;
+}
+
+long double factorial(double lim) {
+    long double fact = 1;
+    for (long double count = 1; count <= lim; count++) {
+        fact *= count;
+    }
+    return fact;
 }
